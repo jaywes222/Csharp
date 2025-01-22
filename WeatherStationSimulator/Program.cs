ef8c44e3e7bb58e348c55633df144a907ff50d21
jaywes222 <wesleyjay202@gmail.com>
2025-01-22 11:09:16 +0300

    Setup & Config

---
+++ WeatherStationSimulator/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace WeatherStationSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The No Of Days To Simulate");
+            int days = int.Parse(Console.ReadLine());
+
+            int[] temp = new int[days];
+            string[] conditions = { "Sunny", "Rainy", "Cloudy", "Snowy" };
+            string[] weatherConditions = new string[days];
+
+            Random random = new Random();
+
+            for (int i = 0; i < days; i++)
+            {
+                temp[i] = random.Next(-10, 40);
+                weatherConditions[i] = conditions[random.Next(conditions.Length)];
+            }
+
+        }
+    }
+}
